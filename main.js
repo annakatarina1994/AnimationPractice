e9fffe5258953c63a29632bb125886be3f791c4b
Anna Lizon <annakatarina1994@gmail.com>
2020-07-25 12:16:47 -0400

    Got one of the div elements to animate properly!

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 const menuBtn = document.querySelector('.menu-btn');
+const contactBtn = document.querySelector('.contact');
 let menuOpen = false;
 
 menuBtn.addEventListener('click', ()=>{
   if(!menuOpen){
     menuBtn.classList.add('open');
+    contactBtn.classList.add('open');
     menuOpen = true;
   } else {
     menuBtn.classList.remove('open');
+    contactBtn.classList.remove('open');
+    contactBtn.classList.add('close');
     menuOpen = false;
   }
 });
